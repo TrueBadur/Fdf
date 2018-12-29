194f50c47c8dfafe95cfa1d4da8f0351c3025ec8
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2018-12-30 01:42:47 +0300

    Fix ft_vecpush: now it adds data to the end of the vector, increasing vector capacity if needed

--- libft/ft_vecpush.c
+++ libft/ft_vecpush.c
@@ -1,32 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_vecpush.c                                       :+:      :+:    :+:   */
+/*   ft_vecshrink.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/30 15:52:30 by ehugh-be          #+#    #+#             */
-/*   Updated: 2018/12/04 02:16:33 by ehugh-be         ###   ########.fr       */
+/*   Created: 2018/12/03 17:58:28 by ehugh-be          #+#    #+#             */
+/*   Updated: 2018/12/29 23:17:47 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_vector	*ft_vecpush(t_vector *vec)
+t_vector	*ft_vecpush(t_vector *vec, void *data, size_t s)
 {
-	void	*data_c;
+	void *tmp;
+	int		it;
 
 	if (!vec)
-		return (NULL);
-	data_c = vec->data;
-	if (!(vec->data = malloc(vec->cap * 2)))
-	{
-		free(data_c);
-		free(vec);
-		return (NULL);
-	}
-	vec->cap *= 2;
-	ft_memcpy(vec->data, data_c, vec->len);
-	free(data_c);
+		return (t_vector*)(NULL);
+	if (!data || !s)
+		return (vec);
+	it = vec->cap;
+	while (s > vec->cap - vec->len)
+		it *= 2;
+	if (it > vec->cap)
+		vec = ft_vecgrow(vec, it);
+	ft_memcpy(vec->data + vec->len, data, s);
+	vec->len += s;
+	vec->cap = it;
 	return (vec);
 }
