9f36dbf5b686525766492ea6fae3aebd48f70846
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2018-12-30 01:44:35 +0300

    Fix ft_vecdel: now may take function to free elements of vector, but it works WRONG

--- libft/ft_vecdel.c
+++ libft/ft_vecdel.c
@@ -6,17 +6,20 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 13:32:17 by ehugh-be          #+#    #+#             */
-/*   Updated: 2018/12/07 02:14:24 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2018/12/30 01:11:38 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_vecdel(void **vect)
+void	ft_vecdel(t_vector **vect, void (*f)(void *))
 {
 	if (!vect || !*vect)
 		return ;
-	free(((t_vector *)(*vect))->data);
+	if (!f)
+		free((*vect)->data);
+	else
+		f((*vect)->data);
 	free(*vect);
 	*vect = NULL;
 }
