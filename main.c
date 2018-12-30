18bc704784a0ababcd4fa2ac8d0ad733eb21c0b0
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2018-12-30 05:50:19 +0300

    Fix main: comment out all except get_map

--- main.c
+++ main.c
@@ -6,10 +6,11 @@
 /*   By: bparker <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/23 11:17:51 by bparker           #+#    #+#             */
-/*   Updated: 2018/12/29 19:59:43 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2018/12/30 05:39:50 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "fdf.h"
 
 void	drawlineeasy(double x0, double x1, double y0, double y1, void *mlx_ptr, void *win_ptr, int color)
@@ -21,8 +22,6 @@ void	drawlineeasy(double x0, double x1, double y0, double y1, void *mlx_ptr, voi
 	dx = (x1 - x0) / (y1 - y0);
 	if (y1 < y0)
 		dx *= -1;
-	if (color == 0x0000FF)
-		printf("BLUE, %f", dx);
 	dy = (y1 - y0) / (x1 - x0);
 	if (x1 < x0)
 		dy *= -1;
@@ -53,8 +52,6 @@ void	drawlineeasy(double x0, double x1, double y0, double y1, void *mlx_ptr, voi
 int		hook_keydown(int key, void *param)
 {
 	(void)param;
-	if (key == 12)
-		printf("Hello\n");
 	if (key == 53)
 		exit(1);
 	return (0);
@@ -62,17 +59,24 @@ int		hook_keydown(int key, void *param)
 
 int		main(int ac, char **av)
 {
-	void		*mlx_ptr;
-	void		*win_ptr;
-	t_vector	*map;
+//	void	*mlx_ptr;
+//	void	*win_ptr;
+	t_map	*map;
 //	t_coords	xy;
 
 	if (ac == 2)
 	{
+		printf("HELLO!\n");
 		map = ft_get_map(av[1]);
-		ft_rotate(map);
-		mlx_ptr = mlx_init();
-		win_ptr = mlx_new_window(mlx_ptr, 1024, 1024, "SuperTittle");
+		printf("map = %p\n", map);
+		t_vec4 *po = (t_vec4*)(map->vec->data);
+		for (int i = map->w * map->h; i; i--)
+		{
+			printf("point %d (x = %f, y = %f, z = %f, col = %X)\n", i, (po[i]).x, (po[i]).y, (po[i]).z, (po[i]).color);
+		}
+	//	ft_rotate(map);
+//		mlx_ptr = mlx_init();
+//		win_ptr = mlx_new_window(mlx_ptr, 1024, 1024, "SuperTittle");
 		/*drawlineeasy(100, 150, 100, 200, mlx_ptr, win_ptr, 0xFF0000);
 		drawlineeasy(100, 150, 100, 0, mlx_ptr, win_ptr, 0x00FF00);
 		drawlineeasy(200, 150, 100, 200, mlx_ptr, win_ptr, 0xFFFFFF);
@@ -83,8 +87,8 @@ int		main(int ac, char **av)
 		drawlineeasy(650, 500, 100, 200, mlx_ptr, win_ptr, 0xFFFFFF);
 		drawlineeasy(850, 1000, 100, 100, mlx_ptr, win_ptr, 0xFFFFFF);
 		drawlineeasy(950, 950, 100, 200, mlx_ptr, win_ptr, 0xFFFFFF);*/
-		mlx_key_hook(win_ptr, hook_keydown, (void *)0);
-		mlx_loop(mlx_ptr);
+//		mlx_key_hook(win_ptr, hook_keydown, (void *)0);
+//		mlx_loop(mlx_ptr);
 	}
 	return (0);
 }
