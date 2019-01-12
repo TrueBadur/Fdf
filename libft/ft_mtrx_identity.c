f0c175cceca95e3f7dee71ab8af24dcba0805006
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-01-12 18:24:43 +0300

    fix ft_mtrx_identity: initialize it with 0 before use

--- libft/ft_mtrx_identity.c
+++ libft/ft_mtrx_identity.c
@@ -6,7 +6,7 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/11 07:22:29 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/01/11 07:33:54 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2019/01/12 12:21:16 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,7 @@ t_mtrx	*ft_mtrx_ident(int w, unsigned char el_size)
 
 	if (!w)
 		return (NULL);
-	if (!(ret = malloc(sizeof(t_mtrx))))
-		return (NULL);
-	if (el_size == MTRX_INT)
-		t = sizeof(int);
-	else
-		t = sizeof(double);
-	if (!(ret->mtrx = malloc(w * w * t)))
-	{
-		free(ret);
-		return (NULL);
-	}
-	ret->w = w;
-	ret->h = w;
-	ret->el_size = el_size;
+	ret = ft_mtrx_init(w, w, el_size);
 	t = w;
 	if (el_size == MTRX_DOUBLE)
 		while (t--)
